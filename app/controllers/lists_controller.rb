bfc92c01ce0cb494f98fcc6c04f12b25f12a7501
leo filipetti <leo.filipetti@gmail.com>
2021-05-13 17:05:34 +0200

    movies

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,41 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update]
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @list.update(list_params)
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
